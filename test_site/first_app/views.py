949a31dfd255189cdc088f26cf8115a7c178d450
VaporDusk <vapordusk@mail.ru>
2022-07-09 15:50:04 +0500

    Add view function 'second_function'

--- test_site/first_app/views.py
+++ test_site/first_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Страница приложения first_app.")
+
+def second_function(request):
+    return HttpResponse("<h1>Заголовок страницы</h1>")
